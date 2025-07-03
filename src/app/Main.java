e930d22b6bf92ca248605671131d46f3cddcdf72
BL4CK DoG <139675702+xoxohoon01@users.noreply.github.com>
2025-07-03 10:38:33 +0900

    Feat: main 함수에서 콘솔 i/o 연결

---
+++ src/app/Main.java
@@ -0,0 +1,16 @@
+package app;
+
+import io.ConsoleInputProvider;
+import io.ConsoleOutputRenderer;
+import io.InputProvider;
+import io.OutputRenderer;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        InputProvider input = new ConsoleInputProvider();
+        OutputRenderer output = new ConsoleOutputRenderer();
+
+    }
+}
